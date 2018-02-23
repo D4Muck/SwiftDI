3f31c99f444cd65babf6d11727911997c112a936
Christoph Muck <muckz11@gmail.com>
2018-02-23 10:22:41 +0100

    Fix copyright header

--- SwiftDI/FrameworkClasses.swift
+++ SwiftDI/FrameworkClasses.swift
@@ -1,6 +1,6 @@
 //
 // Created by Christoph Muck on 11.02.18.
-// Copyright (c) 2018 AppFactory GmbH. All rights reserved.
+// Copyright (c) 2018 Christoph Muck. All rights reserved.
 //
 
 import Foundation
