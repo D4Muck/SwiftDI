44c7f44964fcd92069a687b006457ec76bd21074
Christoph Muck <muckz11@gmail.com>
2018-03-01 10:59:48 +0100

    Fix error where scoped dependency was not created

--- SwiftDI/FilesToInclude/79_Component_Calculations.swift
+++ SwiftDI/FilesToInclude/79_Component_Calculations.swift
@@ -5,21 +5,101 @@
 
 import Foundation
 
+class ComponentToCalculate {
+    let name: String
+    let module: String
+    let methods: [ComponentMethod]
+    let includedModules: [Module]
+    let scope: String?
+    let parent: String?
+    var requiredTypeNames: [String]! = [String]()
+
+    init(
+        name: String,
+        module: String,
+        methods: [ComponentMethod],
+        includedModules: [Module],
+        scope: String?,
+        parent: String?
+    ) {
+        self.name = name
+        self.module = module
+        self.methods = methods
+        self.includedModules = includedModules
+        self.scope = scope
+        self.parent = parent
+    }
+}
+
 func calculateComponents() -> [Component] {
     let allModules = getAllModules()
     let subcomponentGraph = calculateSubcomponentParents(from: allModules)
-    var components = getComponentAndSubcomponentTypes().map { type -> Component in
+    var componentsToCalculate: [ComponentToCalculate] = getComponentsToCalculate(allModules, subcomponentGraph)
+
+    var components = [Component]()
+    repeat {
+        let newComponents = getComponentsToCalculateThatHaveNoParents(componentsToCalculate)
+            .map { c -> Component in
+                let index = componentsToCalculate.index { $0.name == c.name }
+                componentsToCalculate.remove(at: index!)
+
+                let order = calculateDependencyOrder(forTypeNames: c.requiredTypeNames, andIncludedModules: c.includedModules)
+
+                order.forEach { dependency in
+                    if case .scoped(let scopeName) = dependency.trait,
+                       scopeName != c.scope,
+                       let component = componentsToCalculate.first(where: { $0.scope == scopeName }) {
+                        component.requiredTypeNames.append(dependency.typeName)
+                    }
+                }
+
+
+                return Component(name: c.name, module: c.module, order: order, methods: c.methods, modules: c.includedModules, scope: c.scope)
+            }
+        components.append(contentsOf: newComponents)
+    } while componentsToCalculate.count != 0
+
+    return splitIntoComponentsAndSubcomponents(components, subcomponentGraph: subcomponentGraph)
+}
+
+func getComponentsToCalculate(_ allModules: [Module], _ subcomponentGraph: [String: String]) -> [ComponentToCalculate] {
+    return getComponentAndSubcomponentTypes().map { type -> ComponentToCalculate in
         let methods = type.methods.map { ComponentMethod(name: $0.shortName, typeName: $0.returnTypeName.name) }
         let requiredTypeNames = methods.map { $0.typeName }
         let includedModuleNames = (type.annotations["Modules"] as? String)?.split(separator: ",").map(String.init) ?? []
         let includedModules = includedModuleNames.map { includedModuleName in
             return allModules.first { $0.name == includedModuleName } ?? moduleNotFondError(name: includedModuleName)
         }
-        let order = calculateDependencyOrder(forTypeNames: requiredTypeNames, andIncludedModules: includedModules)
         let scope = type.annotations["Scope"] as? String
-        return Component(name: type.name, module: type.module ?? "", order: order, methods: methods, modules: includedModules, scope: scope)
+        let c = ComponentToCalculate(name: type.name, module: type.module ?? "", methods: methods, includedModules: includedModules, scope: scope, parent: subcomponentGraph[type.name])
+        c.requiredTypeNames.append(contentsOf: requiredTypeNames)
+        return c
     }
+}
 
+func getComponentsToCalculateThatHaveNoParents(_ componentsToCalculate: [ComponentToCalculate]) -> [ComponentToCalculate] {
+    return componentsToCalculate.reduce(into: [String: Int]()) { map, entry in
+            if let _ = map[entry.name] {
+            } else {
+                map[entry.name] = 0
+            }
+
+            if let parent = entry.parent {
+                let newCount: Int
+                if let count = map[parent] {
+                    newCount = count + 1
+                } else {
+                    newCount = 1
+                }
+                map[parent] = newCount
+            }
+        }
+        .filter { (e: (key: String, value: Int)) in e.value == 0 }.map { $0.key }
+        .map { name in componentsToCalculate.first(where: { (c: ComponentToCalculate) in c.name == name }) }.flatMap { $0 }
+}
+
+func splitIntoComponentsAndSubcomponents(_ components: [Component], subcomponentGraph: [String: String]) -> [Component] {
+    var components = components
     let subcomponents = subcomponentGraph.map { entry -> Subcomponent in
         let subcomponentName = entry.key
         let parentName = entry.value
